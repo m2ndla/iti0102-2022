e77d1d5935de01afb197d1dd06bfd6a5084d7dea
Marko <mandla@ttu.ee>
2022-08-31 16:38:52 +0300

    Lahendus v1

--- EX/ex01_hello/greetings.py
+++ EX/ex01_hello/greetings.py
@@ -16,6 +16,6 @@ recipient = input("Enter a recipient:")
 # Asking how many times to repeat.
 repeat_times = int(input("How many times to repeat:"))
 # Defining the final greeting based on input.
-final_greeting = greeting + " " + recipient + " "
+final_greeting = greeting + " " + recipient + "! "
 # Printing the final answer.
 print(final_greeting * repeat_times)
