e98d8250c3a6d3d817b18ca1c156641cc2da9e9f
Marko <mandla@ttu.ee>
2022-09-24 15:00:37 +0300

    part1 lahendus v1

---
+++ EX/ex04_lists/lists.py
@@ -0,0 +1,118 @@
+"""Car inventory. part 1"""
+
+
+def list_of_cars(all_cars: str) -> list:
+    """
+    Return list of cars.
+
+    The input string contains of car makes and models, separated by comma.
+    Both the make and the model do not contain spaces (both are one word).
+
+    "Audi A4,Skoda Superb,Audi A4" => ["Audi A4", "Skoda Superb", "Audi A4"]
+    """
+    return all_cars.split(",")
+print(list_of_cars("Audi A4,BMW 320,BMW 325,Audi A4"))
+
+
+def car_makes(all_cars: str) -> list:
+    """
+    Return list of unique car makes.
+
+    The order of the elements should be the same as in the input string (first appearance).
+
+    "Audi A4,Skoda Superb,Audi A4" => ["Audi", "Skoda"]
+    """
+    all_makes = []
+    cars = all_cars.split(",")
+    for car in cars:
+        unit = car.split(" ")
+        if unit[0] in all_makes:
+            continue
+        else:
+            all_makes.append(unit[0])
+    return all_makes
+print(car_makes("Audi A4,BMW 320,BMW 325,Audi A4"))
+
+def car_models(all_cars: str) -> list:
+    """
+    Return list of unique car models.
+
+    The order of the elements should be the same as in the input string (first appearance).
+
+    "Audi A4,Skoda Superb,Audi A4,Audi A6" => ["A4", "Superb", "A6"]
+    """
+    all_models = []
+    cars = all_cars.split(",")
+    for car in cars:
+        unit = car.split(" ")
+        if unit[1] in all_models:
+            continue
+        else:
+            all_models.append(unit[1])
+    return all_models
+print(car_models("Audi A4,BMW 320,BMW 325,Audi A4"))
+
+
+"""Part 3."""
+
+
+def car_make_and_models(all_cars: str) -> list:
+    """
+    Create a list of structured information about makes and models.
+    For each different car make in the input string an element is created in the output list.
+    The element itself is a list, where the first position is the name of the make (string),
+    the second element is a list of models for the given make (list of strings).
+
+    No duplicate makes or models should be in the output.
+
+    The order of the makes and models should be the same os in the input list (first appearance).
+
+    "Audi A4,Skoda Super,Skoda Octavia,BMW 530,Seat Leon Lux,Skoda Superb,Skoda Superb,BMW x5" =>
+    [['Audi', ['A4']], ['Skoda', ['Super', 'Octavia', 'Superb']], ['BMW', ['530', 'x5']], ['Seat', ['Leon Lux']]]
+    """
+    return []
+
+
+def add_cars(car_list: list, all_cars: str) -> list:
+    """
+    Add cars from the list into the existing car list.
+
+    The first parameter is in the same format as the output of the previous function.
+    The second parameter is a string of comma separated cars (as in all the previous functions).
+    The task is to add cars from the string into the list.
+
+    Hint: This and car_make_and_models are very similar functions. Try to use one inside another.
+
+    [['Audi', ['A4']], ['Skoda', ['Superb']]]
+    and
+    "Audi A6,BMW A B C,Audi A4"
+
+    =>
+
+    [['Audi', ['A4', 'A6']], ['Skoda', ['Superb']], ['BMW', ['A B C']]]
+    """
+    return []
+
+
+"""Part 4."""
+
+
+def number_of_cars(all_cars: str) -> list:
+    """
+    Create a list of tuples with make quantities.
+    The result is a list of tuples.
+    Each tuple is in the form: (make_name: str, quantity: int).
+    The order of the tuples (makes) is the same as the first appearance in the list.
+    """
+    return []
+
+
+def car_list_as_string(cars: list) -> str:
+    """
+    Create a list of cars.
+
+    The input list is in the same format as the result of car_make_and_models function.
+    The order of the elements in the string is the same as in the list.
+    [['Audi', ['A4']], ['Skoda', ['Superb']]] =>
+    "Audi A4,Skoda Superb"
+    """
