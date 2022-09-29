ba41d21a068a9981fbd6ca6dbf0a3826909bd48f
Marko <mandla@ttu.ee>
2022-09-29 15:57:35 +0300

    part 2 lahendus v1

--- EX/ex05_hobbies/hobbies.py
+++ EX/ex05_hobbies/hobbies.py
@@ -49,7 +49,6 @@ def sort_dictionary(dic: dict) -> dict:
     return dic
 
 
-
 if __name__ == '__main__':
     sample_data = """Jack:crafting\nPeter:hiking\nWendy:gaming\nMonica:tennis\nChris:origami\nSophie:sport\nMonica:design\nCarmen:sport\nChris:sport\nMonica:skateboarding\nCarmen:cooking\nWendy:photography\nMonica:tennis\nCooper:yoga\nWendy:sport\nCooper:movies\nMonica:theatre\nCooper:yoga\nChris:gaming\nMolly:fishing\nJack:skateboarding\nWendy:fishing\nJack:drawing\nMonica:baking\nSophie:baking\nAlfred:driving\nAlfred:shopping\nAlfred:crafting\nJack:drawing\nCarmen:shopping\nCarmen:driving\nPeter:drawing\nCarmen:shopping\nWendy:fitness\nAlfred:travel\nJack:origami\nSophie:design\nJack:pets\nCarmen:dance\nAlfred:baking\nSophie:sport\nPeter:gaming\nJack:skateboarding\nCooper:football\nAlfred:sport\nCooper:fitness\nChris:yoga\nWendy:football\nMolly:design\nJack:hiking\nMonica:pets\nCarmen:photography\nJack:baking\nPeter:driving\nChris:driving\nCarmen:driving\nPeter:theatre\nMolly:hiking\nWendy:puzzles\nJack:crafting\nPeter:photography\nCarmen:theatre\nSophie:crafting\nCarmen:cooking\nAlfred:gaming\nPeter:theatre\nCooper:hiking\nChris:football\nChris:pets\nJack:football\nMonica:skateboarding\nChris:driving\nCarmen:pets\nCooper:gaming\nChris:hiking\nJack:cooking\nPeter:fishing\nJack:gaming\nPeter:origami\nCarmen:movies\nSophie:driving\nJack:sport\nCarmen:theatre\nWendy:shopping\nCarmen:pets\nWendy:gaming\nSophie:football\nWendy:theatre\nCarmen:football\nMolly:theatre\nPeter:theatre\nMonica:flowers\nMolly:skateboarding\nPeter:driving\nSophie:travel\nMonica:photography\nCooper:cooking\nJack:fitness\nPeter:cooking\nChris:gaming"""
     dic = create_dictionary(sample_data)
@@ -60,3 +59,33 @@ if __name__ == '__main__':
     print(len(dic["Carmen"]))  # -> 10
     print(len(dic["Molly"]))  # -> 5
     print(len(dic["Sophie"]))  # -> 7
+
+
+"""Hobbies part 2."""
+
+
+def create_dictionary_with_hobbies(data: str) -> dict:
+    """
+    Create dictionary about hobbies and their hobbyists ie. {hobby1: [name1, name2, ...], hobby2: [...]}.
+    :param data: given string from database
+    :return: dictionary, where keys are hobbies and values are lists of people. Values are sorted alphabetically
+    """
+    dictionary = {}
+    hobby_list = []
+    person_and_hobby = data.split("\n")
+    for person in person_and_hobby:
+        person_split = person.split(":")
+        if not hobby_list:
+            hobby_list.append([person_split[1], [person_split[0]]])
+        else:
+            for existing_hobby in hobby_list:
+                if person_split[1] == existing_hobby[0]:
+                    existing_hobby[1].append(person_split[0])
+                    break
+            else:
+                hobby_list.append([person_split[1], [person_split[0]]])
+    for hobby in hobby_list:
+        dictionary[hobby[0]] = list(set(hobby[1]))
+    return sort_dictionary(dictionary)
+
+print(create_dictionary_with_hobbies(sample_data))
