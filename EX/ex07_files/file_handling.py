14d2ad25d348696b0b10f6b951afcf0cac53a3a2
Marko <mandla@ttu.ee>
2022-10-17 22:55:03 +0300

    part 1 lahendus

--- EX/ex07_files/file_handling.py
+++ EX/ex07_files/file_handling.py
@@ -208,8 +208,8 @@ def merge_dates_and_towns_into_csv(dates_filename: str, towns_filename: str, csv
             if item[0] == line[0]:
                 item[1] = line[1]
                 break
-            else:
-                result.append([line[0], line[1], "-"])
-                break
+        else:
+            result.append([line[0], line[1], "-"])
+            break
     write_csv_file(csv_output_filename, result)
     pass
