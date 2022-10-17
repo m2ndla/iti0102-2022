c9a34f2ccafda779675df5583472223a6e6bf99b
Marko <mandla@ttu.ee>
2022-10-18 00:38:13 +0300

    part 2 poolik lahendus

--- EX/ex07_files/file_handling.py
+++ EX/ex07_files/file_handling.py
@@ -212,3 +212,104 @@ def merge_dates_and_towns_into_csv(dates_filename: str, towns_filename: str, csv
             result.append([line[0], line[1], "-"])
     write_csv_file(csv_output_filename, result)
     pass
+
+
+"""Part 2."""
+
+
+def create_dictionary(name: str, age: str, sex: str) -> dict:
+    """
+    Helping function: Creates dictionary in suitable form.
+
+    :param name: Name.
+    :param age: Age.
+    :param sex: Sex.
+    :return: Returns dict in correct form.
+    """
+    dct = {"name": name, "age": age, "sex": sex}
+    return dct
+
+
+def read_csv_file_into_list_of_dicts(filename: str) -> list:
+    """
+    Read csv file into list of dictionaries.
+
+    Header line will be used for dict keys.
+
+    Each line after header line will result in a dict inside the result list.
+    Every line contains the same number of fields.
+
+    Example:
+    name,age,sex
+    John,12,M
+    Mary,13,F
+
+    Header line will be used as keys for each content line.
+    The result:
+    [
+      {"name": "John", "age": "12", "sex": "M"},
+      {"name": "Mary", "age": "13", "sex": "F"},
+    ]
+
+    If there are only header or no rows in the CSV-file,
+    the result is an empty list.
+
+    The order of the elements in the list should be the same
+    as the lines in the file (the first line becomes the first element etc.)
+
+    :param filename: CSV-file to read.
+    :return: List of dictionaries where keys are taken from the header.
+    """
+    result = []
+    file = read_csv_file(filename)
+    for person in file[1:]:
+        name = person[0]
+        age = person[1]
+        sex = person[2]
+        result.append(create_dictionary(name, age, sex))
+    return result
+
+
+def write_list_of_dicts_to_csv_file(filename: str, data: list) -> None:
+    """
+    Write list of dicts into csv file.
+
+    Data contains a list of dictionaries.
+    Dictionary key represents the field.
+
+    Example data:
+    [
+      {"name": "john", "age": "23"}
+      {"name": "mary", "age": "44"}
+    ]
+    Will become:
+    name,age
+    john,23
+    mary,44
+
+    The order of fields/headers is not important.
+    The order of lines is important (the same as in the list).
+
+    Example:
+    [
+      {"name": "john", "age": "12"},
+      {"name": "mary", "town": "London"}
+    ]
+    Will become:
+    name,age,town
+    john,12,
+    mary,,London
+
+    Fields which are not present in one line will be empty.
+
+    The order of the lines in the file should be the same
+    as the order of elements in the list.
+
+    :param filename: File to write to.
+    :param data: List of dictionaries to write to the file.
+    :return: None
+    """
+    pass
+
+
+read_csv_file_into_list_of_dicts("text.txt")
