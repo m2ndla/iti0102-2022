1f8e8751f8f0641bcef5d73c63258a61d07ee662
Marko <mandla@ttu.ee>
2022-10-19 23:13:57 +0300

    tester v6

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -109,3 +109,6 @@ def test_fruit_order__other():
     assert fruit_order(29, 32, 948) == -1
     assert fruit_order(72, 72, 432) == 72
     assert fruit_order(0, 5, 22) == -1
+    assert fruit_order(999, 999, 5994) == 999
+
+
