c3770c83e3cc75f6c087490c49af0bd7824df28b
Marko <mandla@ttu.ee>
2022-10-19 21:23:27 +0300

    tester v2

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -6,16 +6,20 @@ from solution import students_study
 from solution import lottery
 from solution import fruit_order
 
+
 def test_students_study__evening_coffee_doesnt_matter():
-    assert students_study(19, True) is True
-    assert students_study(19, False) is True
+    """Test whether drinking coffee matters when studying in the evening."""
+    assert students_study(18, True) is True
+    assert students_study(24, False) is True
 
 
 def test_students_study__daytime_coffee_matters():
-    assert students_study(12, True) is True
-    assert students_study(12, False) is False
+    """Test if drinking coffee affects studying in the daytime."""
+    assert students_study(5, True) is True
+    assert students_study(17, False) is False
 
 
 def test_students_study__nighttime_no_study():
+    """Test whether students study at night."""
     assert students_study(1, True) is False
     assert students_study(4, False) is False
