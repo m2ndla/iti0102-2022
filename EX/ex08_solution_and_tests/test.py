c17b55f9e14df0cd9b6b4f3bd53fd1a589d4b078
Marko <mandla@ttu.ee>
2022-10-19 21:33:57 +0300

    tester v3

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -28,10 +28,40 @@ def test_students_study__evening_coffee_doesnt_matter():
 def test_students_study__daytime_coffee_matters():
     """Test if drinking coffee affects studying in the daytime."""
     assert students_study(5, True) is True
+    assert students_study(6, True) is True
+    assert students_study(7, True) is True
+    assert students_study(8, True) is True
+    assert students_study(9, True) is True
+    assert students_study(10, True) is True
+    assert students_study(11, True) is True
+    assert students_study(12, True) is True
+    assert students_study(13, True) is True
+    assert students_study(14, True) is True
+    assert students_study(15, True) is True
+    assert students_study(16, True) is True
+    assert students_study(17, True) is True
+    assert students_study(5, False) is False
+    assert students_study(6, False) is False
+    assert students_study(7, False) is False
+    assert students_study(8, False) is False
+    assert students_study(9, False) is False
+    assert students_study(10, False) is False
+    assert students_study(11, False) is False
+    assert students_study(12, False) is False
+    assert students_study(13, False) is False
+    assert students_study(14, False) is False
+    assert students_study(15, False) is False
+    assert students_study(16, False) is False
     assert students_study(17, False) is False
 
 
 def test_students_study__nighttime_no_study():
     """Test whether students study at night."""
     assert students_study(1, True) is False
+    assert students_study(2, True) is False
+    assert students_study(3, True) is False
+    assert students_study(4, True) is False
+    assert students_study(1, False) is False
+    assert students_study(2, False) is False
+    assert students_study(3, False) is False
     assert students_study(4, False) is False
