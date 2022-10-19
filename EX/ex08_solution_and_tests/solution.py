d9e92970088784c77aa2b68ef6c80fbd88bf7cac
Marko <mandla@ttu.ee>
2022-10-19 20:34:57 +0300

    solution lahendus

--- EX/ex08_solution_and_tests/solution.py
+++ EX/ex08_solution_and_tests/solution.py
@@ -42,7 +42,8 @@ def fruit_order(small_baskets: int, big_baskets: int, ordered_amount: int) -> in
     (3, 1, 10) -> -1
     """
     most_big_baskets = []
-    for number in range(1, big_baskets + 1):
+    bsk = big_baskets + 1
+    for number in range(1, bsk):
         if number * 5 < ordered_amount:
             most_big_baskets.append(number)
         else:
