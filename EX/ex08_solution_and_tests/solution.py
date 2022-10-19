47fc0a707272ced40308064de3b9d0fceeed4752
Marko <mandla@ttu.ee>
2022-10-19 20:11:16 +0300

    solution lahendus

---
+++ EX/ex08_solution_and_tests/solution.py
@@ -0,0 +1,54 @@
+"""EX08 - Solution and tests."""
+
+
+def students_study(time: int, coffee_needed: bool) -> bool:
+    """
+    Return True if students study in given circumstances.
+
+    (19, False) -> True
+    (1, True) -> False.
+    """
+    if time in range(18, 25) or (time in range(5, 18) and coffee_needed):
+        return True
+    elif time in range(1, 5):
+        return False
+    else:
+        return False
+
+
+def lottery(a: int, b: int, c: int) -> int:
+    """
+    Return Lottery victory result 10, 5, 1, or 0 according to input values.
+
+    (5, 5, 5) -> 10
+    (2, 2, 1) -> 0
+    (2, 3, 1) -> 1
+    """
+    if a == b == c == 5:
+        return 10
+    if a == b == c != 5:
+        return 5
+    if a != b and a != c:
+        return 1
+    elif a == b or a == c:
+        return 0
+
+
+def fruit_order(small_baskets: int, big_baskets: int, ordered_amount: int) -> int:
+    """
+    Return number of small fruit baskets if it's possible to finish the order, otherwise return -1.
+
+    (4, 1, 9) -> 4
+    (3, 1, 10) -> -1
+    """
+    if big_baskets >= (ordered_amount / 5):
+        if small_baskets >= (ordered_amount % 5):
+            return small_baskets
+        else:
+            if big_baskets > (ordered_amount / 5):
+                return small_baskets
+    else:
+        if small_baskets >= int(ordered_amount - big_baskets * 5):
+            return small_baskets
+        else:
+            return -1
