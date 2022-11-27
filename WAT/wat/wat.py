bbda0e28e67051ed11db4a29f39efc127faad160
Marko <mandla@ttu.ee>
2022-11-27 23:00:20 +0200

    ???

--- WAT/wat/wat.py
+++ WAT/wat/wat.py
@@ -1,9 +1,41 @@
 """Wat."""
 
 
-def wat():
-    """Wat."""
-    return "wat."
+def function_a():
+    """I have no clue."""
+    return 1
 
 
-print("wat???")
+def function_b():
+    """I have no clue."""
+    return 2
+
+
+def function_c():
+    """I have no clue."""
+    return 3
+
+
+def function_d():
+    """I have no clue."""
+    return 4
+
+
+def function_e():
+    """I have no clue."""
+    return 5
+
+
+def function_f():
+    """I have no clue."""
+    return 6
+
+
+def function_g():
+    """I have no clue."""
+    return 7
+
+
+def function_h():
+    """I have no clue."""
+    return 8
